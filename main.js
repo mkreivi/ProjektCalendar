ca3b17db92e6da38e3a4749a1f567d432be26450
Martina Kreivi <martinakreivi@gmail.com>
2022-12-17 10:13:05 +0100

    Setting up calendar in JS

--- main.js
+++ main.js
@@ -1,5 +1,67 @@
-window.addEventListener('DOMContentLoaded', main);
+window.addEventListener('DOMContentLoaded', load);
 
-function main() {
-    
-}
+let nav = 0;
+let clicked = null;
+let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];
+
+const calendar = document.getElementById('calendar');
+const weekdays = ['Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lördag', 'Söndag'];
+
+
+function load() {
+    const dt = new Date();
+
+    if(nav !== 0) {
+        dt.setMonth(new Date().getMonth() + nav);
+    }
+   
+    const day = dt.getDate();
+    const month = dt.getMonth();
+    const year = dt.getFullYear();
+
+    const firstDayOfMonth = new Date(year, month, 1);
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+    const dateString = firstDayOfMonth.toLocaleDateString('sv', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+    });
+    const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
+
+    document.getElementById('monthDisplay').innerText = 
+    `${dt.toLocaleDateString('sv', { month: 'long'})} ${year}`;
+
+    calendar.innerHTML = '';
+
+    for(let i = 1; i <= paddingDays + daysInMonth; i++) {
+        const daySquare = document.createElement('div');
+        daySquare.classList.add('day');
+
+        if (i > paddingDays) {
+            daySquare.innerText = i - paddingDays;
+
+            daySquare.addEventListener('click', () => console.log('click'));
+        } else {
+            daySquare.classList.add('padding');
+        }
+
+        calendar.appendChild(daySquare);
+        }
+
+    }
+
+    function initButtons() {
+        document.getElementById('nextButton').addEventListener('click', () => {
+            nav++;
+            load();
+        });
+
+        document.getElementById('backButton').addEventListener('click', () => {
+            nav--;
+            load();
+        });
+    }
+initButtons();
+load();
